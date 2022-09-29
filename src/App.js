146d87bf09220a87d754a502b27f279b587a36b8
uziriahimu <uziriahimu@gmail.com>
2022-09-29 09:28:32 +0600

    responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card/Card';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
